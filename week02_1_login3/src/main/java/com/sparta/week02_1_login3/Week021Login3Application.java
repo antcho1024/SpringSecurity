96da8fba06c2159a445df3126d2735c1e3e7c39f
hyeppy <50614113+antcho1024@users.noreply.github.com>
2022-10-07 22:31:49 +0900

    Security 설정, 기본UI

---
+++ week02_1_login3/src/main/java/com/sparta/week02_1_login3/Week021Login3Application.java
@@ -0,0 +1,13 @@
+package com.sparta.week02_1_login3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week021Login3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week021Login3Application.class, args);
+    }
+
+}
---
+++ week02_1_login3/src/test/java/com/sparta/week02_1_login3/Week021Login3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.week02_1_login3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week021Login3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
